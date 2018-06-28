e2d9bb6e792ecb690bd0c2a045bb1a757ffd1ae9
ttguo <tiantian.guo@starasiatrading.com>
2018-06-28 14:31:07 +0800

    1@0628

---
+++ leetcode/1.two-sum.py
@@ -0,0 +1,18 @@
+#!C:\Python27\Python
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            if (target - nums[i] in nums) and (i != nums.index(target - nums[i])):
+                return [i, nums.index(target - nums[i])]
+
+if __name__ == '__main__':
+    print Solution().twoSum([2,3,7],5)
+
+        
+        
