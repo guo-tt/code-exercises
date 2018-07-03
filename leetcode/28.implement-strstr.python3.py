852de303fe2e4185d86aa09cb22443fe6211d73e
ttguo <tiantian.guo@starasiatrading.com>
2018-07-03 13:55:28 +0800

    28@0703

--- leetcode/28.implement-strstr.python3.py
+++ leetcode/28.implement-strstr.python3.py
@@ -44,4 +44,21 @@ class Solution:
         :type needle: str
         :rtype: int
         """
-        
+        flag = 1
+        if len(needle) == 0:
+            return 0
+        if len(haystack) == 0:
+            return -1
+        for i in range(len(haystack)):
+            if i + len(needle) > len(haystack):
+                return -1
+            else:
+                if haystack[i:i+len(needle)] == needle:
+                    return i
+                else:
+                    flag *= 0
+        if flag == 0:
+            return -1        
+
+if __name__ == "__main__":
+    print(Solution().strStr("mississipi","a"))
