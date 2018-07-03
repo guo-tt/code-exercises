f5a4991d042c0fb95e0d07fba7bf8000f41751df
ttguo <tiantian.guo@starasiatrading.com>
2018-07-03 09:35:23 +0800

    21@0703

--- leetcode/21.merge-two-sorted-lists.python3.py
+++ leetcode/21.merge-two-sorted-lists.python3.py
@@ -34,17 +34,10 @@ class Solution:
         """
         if not l1 or not l2:
             return l1 or l2
-        head = cur = ListNode(0)
-
-        while l1 and l2:
-            if l1.val < l2.val:
-                cur.next = l1
-                l1 = l1.next
-            else:
-                cur.next = l2
-                l2 = l2.next
-            cur = cur.next
-        cur.next = l1 or l2
-        return head.next
-
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
                         
