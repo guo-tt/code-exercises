1b151bec611704ee0da8fe646ea78980319cc8a0
guo-tt <guotiantian@spgroup.com.sg>
2019-09-06 10:41:37 +0800

    Tree Operation

---
+++ leetcode/572.subtree-of-another-tree.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=572 lang=python3
+#
+# [572] Subtree of Another Tree
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSubtree(self, s, t):
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+        return self.isSameTree(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        
+    def isSameTree(self, s, t):
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+        return s.val == t.val and self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
+
+        
+
