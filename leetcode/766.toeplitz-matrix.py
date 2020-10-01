7d92c211f5a6ecfa37a5564a45f930cf033086e2
guo-tt <guotiantian@spgroup.com.sg>
2020-10-01 15:52:37 +0800

    766@10012020

---
+++ leetcode/766.toeplitz-matrix.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=766 lang=python3
+#
+# [766] Toeplitz Matrix
+#
+
+# @lc code=start
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+        for i in range(len(matrix) - 1):
+        	for j in range(len(matrix[0]) - 1):
+        		if matrix[i][j] != matrix[i+1][j+1]:
+        			return False
+        return True
+
+# @lc code=end
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isToeplitzMatrix([[1,2],[2,2]]))
